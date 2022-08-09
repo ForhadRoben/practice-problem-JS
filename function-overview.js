f83a4eece481483fbd91080a3dc582699e15c20f
ForhadRobe <i.rforhad@gmail.com>
2022-08-10 01:14:55 +0600

    coding practice  commited

---
+++ function-overview.js
@@ -0,0 +1,51 @@
+/* //function declaration
+function startFan() {
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+startFan();//function call
+console.log('Waking up in the morning')
+startFan(); */
+
+//parameter declaration
+/* function bringSingara(money) {
+    console.log('taka disen: ', money);
+    console.log('ai nen singara');
+}
+
+var taka = 300;
+
+bringSingara(taka); */
+
+/* function add(num1, num2) {
+    console.log('going to add:', num1, num2);
+}
+
+add(125, 96); */
+
+/* function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15, 98, 56, 5, 9); */
+
+//return value
+
+function add(number1, number2) {
+    console.log(number1, number2);
+    var sum = number1 + number2;
+    // console.log(sum);
+    return sum;
+    console.log('I need more code');
+    return 15;
+    return 'hello done';
+    return 'I am hungry';
+}
+
+// add(45, 15);
+
+var total = add(80, 20);
+console.log('total', total);
